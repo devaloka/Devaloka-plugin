ecb5553f568c3a8bd10982768b4f2d44b1f948ee
Whizark <devaloka@whizark.com>
2015-12-13 20:36:45 +0900

    chore: bump version to 0.6.0

--- devaloka-plugin.php
+++ devaloka-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Plugin
 Description: Provides Interfaces, Traits, and Classes for WordPress Plugin
-Version: 0.5.0
+Version: 0.6.0
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
