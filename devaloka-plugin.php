15f91365e3904d86c5ca4a4de9ee3fe444d7933b
Whizark <devaloka@whizark.com>
2015-12-27 00:07:56 +0900

    fix: incorrect version number in plugin header comment

--- devaloka-plugin.php
+++ devaloka-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Plugin
 Description: Provides Interfaces, Traits, and Classes for WordPress Plugin
-Version: 0.6.0
+Version: 0.6.1
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
